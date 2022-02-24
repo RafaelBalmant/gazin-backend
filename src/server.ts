93900b2e69e29f794de8141c112639cf4b810b1d
RafaelBalmant <40277302+RafaelBalmant@users.noreply.github.com>
2022-02-24 19:12:33 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,6 @@ app.use(routes);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof Error) {
-      console.log(err.message);
       return response.status(handleErrorMessages[err.code]?.code || 500).json({
         message:
           handleErrorMessages[err.code]?.message ||
